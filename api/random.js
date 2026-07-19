05cdbcf1758a565c184c0a6caa9e956943729224
fmax39894-maker <fmax39894@gmail.com>
2026-07-19 16:40:51 +0530

    Update random.js

--- api/random.js
+++ api/random.js
@@ -1,1 +1,36 @@
+import axios from "axios";
 
+const OWNER = "fmax39894-maker";
+const REPO = "Tele-botx";
+const BRANCH = "main";
+
+export default async function handler(req, res) {
+  try {
+    const url = `https://api.github.com/repos/${OWNER}/${REPO}/contents`;
+
+    const { data } = await axios.get(url);
+
+    const images = data.filter(file =>
+      /\.(jpg|jpeg|png|gif|webp)$/i.test(file.name)
+    );
+
+    if (images.length === 0) {
+      return res.status(404).json({
+        error: "No images found"
+      });
+    }
+
+    const random = images[Math.floor(Math.random() * images.length)];
+
+    const imageUrl = `https://raw.githubusercontent.com/${OWNER}/${REPO}/${BRANCH}/${random.name}`;
+
+    res.status(200).json({
+      image: imageUrl
+    });
+
+  } catch (e) {
+    res.status(500).json({
+      error: e.message
+    });
+  }
+}
