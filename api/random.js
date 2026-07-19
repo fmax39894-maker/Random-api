a75925e946207e03c0fd0b583fb74914ecc76911
fmax39894-maker <fmax39894@gmail.com>
2026-07-19 17:16:05 +0530

    Update random.js

--- api/random.js
+++ api/random.js
@@ -1,16 +1,16 @@
-import axios from "axios";
-
-const OWNER = "fmax39894-maker";
-const REPO = "Tele-botx";
-const BRANCH = "main";
-
 export default async function handler(req, res) {
+  const OWNER = "fmax39894-maker";
+  const REPO = "Tele-botx";
+  const BRANCH = "main";
+
   try {
-    const url = `https://api.github.com/repos/${OWNER}/${REPO}/contents`;
+    const response = await fetch(
+      `https://api.github.com/repos/${OWNER}/${REPO}/contents`
+    );
 
-    const { data } = await axios.get(url);
+    const files = await response.json();
 
-    const images = data.filter(file =>
+    const images = files.filter(file =>
       /\.(jpg|jpeg|png|gif|webp)$/i.test(file.name)
     );
 
@@ -22,15 +22,16 @@ export default async function handler(req, res) {
 
     const random = images[Math.floor(Math.random() * images.length)];
 
-    const imageUrl = `https://raw.githubusercontent.com/${OWNER}/${REPO}/${BRANCH}/${random.name}`;
+    const imageUrl =
+      `https://raw.githubusercontent.com/${OWNER}/${REPO}/${BRANCH}/${random.name}`;
 
-    res.status(200).json({
+    return res.status(200).json({
       image: imageUrl
     });
 
-  } catch (e) {
-    res.status(500).json({
-      error: e.message
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message
     });
   }
 }
